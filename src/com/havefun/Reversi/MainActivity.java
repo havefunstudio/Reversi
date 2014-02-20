aac3585dc2f7e531c4077d1404be173c85b43936
wangxiaoyu <angiaou@gmail.com>
2014-02-20 22:42:19 +0000

    add to project

---
+++ src/com/havefun/Reversi/MainActivity.java
@@ -0,0 +1,45 @@
+package com.havefun.Reversi;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		
+
+        final Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            	Intent intent = new Intent();  
+            	intent.setClass(MainActivity.this, Game.class);  
+            	startActivity(intent);
+            	
+            }
+        });
+		
+		
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+		
+		
+		
+	}
+
+}
